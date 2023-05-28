c90ddb39dffa896a2918252b68053c3452ce66f4
treecodeXXI <132903579+treecodeXXI@users.noreply.github.com>
2023-05-28 15:14:14 +0200

    Create osc.py

---
+++ src/osc.py
@@ -0,0 +1,12 @@
+class OSC:
+    def __init__(self):
+        self.tics = 0
+        self.freq = 10
+        self.amp = 0
+
+    def run(self):
+        self.tics += 1
+        if freq < self.tics:
+            self.amp ^= 1
+            self.tics=0
+        return self.amp
