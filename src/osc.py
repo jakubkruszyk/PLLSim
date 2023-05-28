ba3060c6e34a4a9fdeaacef14e3bcddeb9784708
Jakub Kruszyk <k.kruszyk7@gmail.com>
2023-05-28 15:19:04 +0200

    Added osc test.

--- src/osc.py
+++ src/osc.py
@@ -1,4 +1,4 @@
-class OSC:
+class Osc:
     def __init__(self):
         self.tics = 0
         self.freq = 10
@@ -8,5 +8,5 @@ class OSC:
         self.tics += 1
         if self.freq < self.tics:
             self.amp ^= 1
-            self.tics=0
+            self.tics = 0
         return self.amp
--- src/pll.py
+++ src/pll.py
@@ -1,20 +1,26 @@
+from src.osc import Osc
+
+
 class PLL:
     """
     Container class for all components creating PLL circuit.
     """
     def __init__(self):
+        self.ref_osc = Osc()
         # Placeholder code
         self.i = 0
         self.a = [1, 1]
 
     def run(self) -> list:
+        x = self.ref_osc.run()
         # Placeholder code
         self.i += 1
         if 40 > self.i > 20:
-            self.a = [0, 0]
+            self.a[0] = 0
         elif self.i > 40:
             self.i = 0
-            self.a = [1, 1]
+            self.a[0] = 1
 
+        self.a[1] = x
         return self.a
 
